7bc7bcd0dd92040d442ad0403f8ca37f9796d9bc
Cyro Gabriel <cyrocunhacg@gmail.com>
2021-03-30 17:54:49 -0300

    3 commit

---
+++ Models/Category.cs
@@ -0,0 +1,18 @@
+
+
+using System.ComponentModel.DataAnnotations;
+
+namespace DataDriven.Models
+{
+    public class Category
+    {
+       
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O campo é obrigatório")]
+        [MaxLength(60, ErrorMessage = "Máximo de 60 caracteres")]
+        [MinLength(3, ErrorMessage = "Mínimo de 3 caracteres")]
+        public string Title { get; set; }
+    }
+}
