e631f57a43d8557e63f3385c396cd7eb1069abb4
Cyro Gabriel <cyrocunhacg@gmail.com>
2021-03-31 16:30:23 -0300

    at

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using DataDriven.Models;
 using Microsoft.AspNetCore.Mvc;
 
 //https://localhost:5001
@@ -7,10 +10,46 @@ namespace DataDriven.Controllers
     [Route("categories")]
     public class CategoryController : ControllerBase
     {
-        [Route("")]
-        public string Function()
+        //---------------GET---------------
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<ActionResult<Category>> GetById(int id)
         {
-            return "Olá mundo";
+            return new Category();
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<ActionResult<List<Category>>> GetById()
+        {
+            return new List<Category>();
+        }
+        //---------------POST---------------
+        [HttpPost]
+        [Route("")]
+        public async Task<ActionResult<List<Category>>> Post([FromBody]Category model)
+        {
+            return Ok(model);
+        }
+
+        //---------------PUT---------------
+        [HttpPut]
+        [Route("{id:int}")]
+        public async Task<ActionResult<List<Category>>> Put(int id, [FromBody]Category model)
+        {
+            if(!ModelState.IsValid) return BadRequest(ModelState);
+            //se o id informado é o msm do modelo
+            if(model.Id == id) return NotFound(new {message = "Categoria não encontrada"});;
+
+            return null;
+        }
+
+        //---------------DELETE---------------
+        [HttpDelete]
+        [Route("{id:int}")]
+        public async Task<ActionResult<List<Category>>> Delete()
+        {
+            return Ok();
         }
     }
 }
